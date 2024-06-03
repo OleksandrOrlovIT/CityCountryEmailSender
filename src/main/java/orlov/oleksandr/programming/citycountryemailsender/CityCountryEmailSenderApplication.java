96b1134030561dc0f929d8199a4df47688c9f5d7
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-06-03 15:35:46 +0300

    Init commit

---
+++ src/main/java/orlov/oleksandr/programming/citycountryemailsender/CityCountryEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package orlov.oleksandr.programming.citycountryemailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCountryEmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityCountryEmailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/orlov/oleksandr/programming/citycountryemailsender/CityCountryEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package orlov.oleksandr.programming.citycountryemailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCountryEmailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
