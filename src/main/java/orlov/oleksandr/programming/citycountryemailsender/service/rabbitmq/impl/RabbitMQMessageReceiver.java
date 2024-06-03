86375435ad219e0c45431fd4eec03011d8856705
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-06-03 20:57:34 +0300

    Added saving to the db from RabbitMQ.

--- src/main/java/orlov/oleksandr/programming/citycountryemailsender/service/rabbitmq/impl/RabbitMQMessageReceiver.java
+++ src/main/java/orlov/oleksandr/programming/citycountryemailsender/service/rabbitmq/impl/RabbitMQMessageReceiver.java
@@ -1,12 +1,46 @@
 package orlov.oleksandr.programming.citycountryemailsender.service.rabbitmq.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import orlov.oleksandr.programming.citycountryemailsender.model.EmailMessage;
+import orlov.oleksandr.programming.citycountryemailsender.model.SendStatus;
+import orlov.oleksandr.programming.citycountryemailsender.service.elasticSearch.EmailMessageService;
 import orlov.oleksandr.programming.citycountryemailsender.service.rabbitmq.MessageReceiver;
 
+import java.util.Map;
+
+@AllArgsConstructor
 @Service
 public class RabbitMQMessageReceiver implements MessageReceiver {
 
+    private static final Logger log = LoggerFactory.getLogger(RabbitMQMessageReceiver.class);
+    private final EmailMessageService emailMessageService;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+        try {
+            Map<String, String> messageContent = objectMapper.readValue(message, Map.class);
+            String subject = messageContent.get("subject");
+            String content = messageContent.get("content");
+            String email = messageContent.get("email");
+
+            EmailMessage emailMessage = EmailMessage.builder()
+                    .subject(subject)
+                    .content(content)
+                    .email(email)
+                    .sendStatus(SendStatus.SENT)
+                    .build();
+
+            EmailMessage savedEmailMessage = emailMessageService.save(emailMessage);
+            log.info("Saved email message {}", savedEmailMessage);
+
+        } catch (JsonProcessingException e) {
+            log.error("Failed to parse message", e);
+        }
     }
 }
